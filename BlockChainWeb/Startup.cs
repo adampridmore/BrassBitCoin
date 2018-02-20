4dac3394edff448ac4c449f15a756583b04242a6
Adam Pridmore <adam_pridmore@hotmail.com>
2018-02-20 20:58:47 +0000

    - Rebuild web site project.

--- BlockChainWeb/Startup.cs
+++ BlockChainWeb/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -29,16 +29,22 @@ namespace BlockChainWeb
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
