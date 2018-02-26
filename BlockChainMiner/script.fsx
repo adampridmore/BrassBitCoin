96baea2998384b2b49cbb501f3859bf5d0bef875
Adam Pridmore <adam_pridmore@hotmail.com>
2018-02-26 20:28:58 +0000

    - Added method to print a block

--- BlockChainMiner/script.fsx
+++ BlockChainMiner/script.fsx
@@ -3,9 +3,11 @@
 
 open BlockChain.Types
 open BlockChain.Miner
+
 let numbeOfBlocksToGenerate = 5
 
 //#time "on"
+
 // genesisBlock |> blockchain numbeOfBlocksToGenerate |> Seq.iter (printfn "%A")
 
 let previousBlockWithHash = {
@@ -20,3 +22,20 @@ let previousBlockWithHash = {
 }
 
 previousBlockWithHash |> blockchain 1 |> Seq.iter (printfn "%A")
+
+let stringReduce seperator (strings: seq<string>) = 
+  strings 
+  |> Seq.reduce (fun a b -> sprintf "%s%s%s" a seperator b)
+
+let printBlock (block: BlockWithHash) = 
+  seq{
+    yield block.block.index |> string
+    yield block.block.minedBy
+    yield block.block.data
+    yield block.block.previousHash
+    yield block.block.nonce |> string
+    yield block.hash
+  } |> (stringReduce " ")
+
+genesisBlock |> printBlock
+
