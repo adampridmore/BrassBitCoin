16fd422c297836bd5e0a5b6ca0821aa43affd54b
Adam Pridmore <adam_pridmore@hotmail.com>
2018-03-04 12:29:44 +0000

    - Fixed mine next block script to load repository and miner assemblies.

--- Code/BlockChainMiner/mine_next_block.fsx
+++ Code/BlockChainMiner/mine_next_block.fsx
@@ -1,27 +1,26 @@
-#load "types.fs"
-#load "minerHelpers.fs"
-#load "miner.fs"
-#load "transactions.fs"
+#I @".\bin\Debug\netstandard2.0"
+#r @"Repository.dll"
+#r @"BlockChain.dll"
 
 open BlockChain.Miner
 open BlockChain.MinerHelpers
 open BlockChain
-open Transaction
 
 let data = 
   //  [   
-        "Transaction,Adam,Dave,5"
+        "Transaction,Dave,Fred,5"
   //  ]
 //    |> stringReduce System.Environment.NewLine
 
-"10 Dave 0 000066FFAD8BC368DACDF4A17EFED490BC36B60A4B7A2E522DCE0A3179F6DC5D 38546 0000D7B3DB4778929CAB77A32B2C11D9A92A59FF70245C519EC21733EB1799BE"
+"11 Dave Transaction,Adam,Dave,5 0000D7B3DB4778929CAB77A32B2C11D9A92A59FF70245C519EC21733EB1799BE 20436 0000439FB6C3954F5E4E77D611BFC566EE964538BAA38F6604C805F1BC7C032F"
 |> parseBlock
 |> Miner.newBlock "Dave" data
 |> sprintBlock
+|> printf "%s"
 
-let miners = (Seq.empty<BlockChain.Types.Miner>)
+//let miners = (Seq.empty<BlockChain.Types.Miner>)
 
-data 
-|> Transaction.parseSingleTransaction
-|> Option.get
-|> Transaction.applyTransactionToMiners miners
+//data 
+//|> Transaction.parseSingleTransaction
+//|> Option.get
+//|> Transaction.applyTransactionToMiners miners
