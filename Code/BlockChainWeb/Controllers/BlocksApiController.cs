c4a022e172a95606fc226b65070fe5c09e28fdbc
Adam Pridmore <adam_pridmore@hotmail.com>
2018-03-05 20:19:34 +0000

    - Added an API to get the latest block.   /api/blocks?latest=true

---
+++ Code/BlockChainWeb/Controllers/BlocksApiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlockChainWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Blocks")]
+    public class BlocksApiController : Controller
+    {
+        private Repository.BlockDtoRepository repository;
+        public BlocksApiController(Repository.BlockDtoRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Get([FromQuery] bool latest)
+        {
+            if (latest != true)
+            {
+                return BadRequest("latest query parameter must be specified and set to true");
+            }
+
+            var block = repository.TryGetLastBlock();
+            if (block == null)
+            {
+                throw new System.ApplicationException("No blocks");
+            }
+
+            var blockWebModel = new Models.BlockWebModel
+            {
+                index = block.index,
+                minedBy = block.minedBy,
+                data = block.data,
+                nonce = block.nonce,
+                previousHash = block.previousHash,
+                hash = block.hash
+            };
+
+            return Json(new List<Models.BlockWebModel>
+            {
+                blockWebModel
+            });
+        }
+    }
+}
---
+++ Code/BlockChainWeb/Models/BlockWebModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlockChainWeb.Models
+{
+    public class BlockWebModel
+    {
+        public int index { get; set; }
+        public string minedBy { get; set; }
+        public string data { get; set; }
+        public string previousHash { get; set; }
+        public int nonce { get; set; }
+        public string hash { get; set; }
+    }
+}
